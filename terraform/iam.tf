4c240e17c541601642d3a1746ef8bfe975b561c5
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-25 13:22:03 +0530

    Re-add IAM role for EC2 and Jenkins

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,52 +17,7 @@
       })
     }
 
-# IAM role for EKS
 
-# EKS Cluster Role
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-# EKS Worker Node Role
-resource "aws_iam_role" "eks_worker_node_role" {
-  name = "eks-worker-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment_1" {
-  role       = aws_iam_role.eks_worker_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
 
 # IAM role for jenkins
 
@@ -85,4 +40,5 @@ resource "aws_iam_role" "jenkins_role" {
   tags = {
     Name = "jenkins-ec2-role"
   }
+
 }
