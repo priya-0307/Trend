6238d6ee7f9ed18d5b761f76aa68f27a03eafdc8
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-11-25 13:19:56 +0530

    Add required_providers block for AWS provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
-provider "aws" {
-region = ap-south-1
+terraform {
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+      region = ap-south-1  
+     }
+  }
 }
+
 
 
 
